11845fc413a0c707cb47c8686c41e517424066a0
jfjsdlfjlsdkfj <roshanchau001@gmail.com>
2023-01-11 23:23:05 +0545

    using fetch api to populate the newsMonkey app

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,7 +8,15 @@ export class NewsItem extends Component {
     return (
       <div>
         <div className="card my-3" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img
+            src={
+              !imageUrl
+                ? "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
