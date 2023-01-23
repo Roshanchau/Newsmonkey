ce4afab4af6165dbe03ead30676abdae98166ac4
jfjsdlfjlsdkfj <roshanchau001@gmail.com>
2023-01-23 18:55:02 +0545

    stickyNavbar and bug fixes

--- src/components/News.js
+++ src/components/News.js
@@ -9,9 +9,6 @@ const News = (props) => {
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(1);
   const [totalResults, setTotalResults] = useState(0);
-  // document.title = `${capitalizeFirstLetter(
-  //   props.category
-  // )}-NewsMonkey`;
 
   const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -36,6 +33,7 @@ const News = (props) => {
   };
 
   useEffect(() => {
+    document.title = `${capitalizeFirstLetter(props.category)}-NewsMonkey`;
     updateNews();
   }, []);
 
@@ -99,9 +97,13 @@ const News = (props) => {
   };
 
   const fetchMoreData = async () => {
-    setPage(page + 1);
     // this.setState({ page: this.state.page + 1 });
-    let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${
+      props.country
+    }&category=${props.category}&apiKey=${props.apiKey}&page=${
+      page + 1
+    }&pageSize=${props.pageSize}`;
+    setPage(page + 1);
     let data = await fetch(url);
     let parsedData = await data.json();
     // console.log(parsedData);
@@ -115,7 +117,10 @@ const News = (props) => {
 
   return (
     <>
-      <h1 className="text-center" style={{ margin: "35px 0" }}>
+      <h1
+        className="text-center"
+        style={{ margin: "35px 0", marginTop: "90px" }}
+      >
         {`NewsMonkey- Top Headlines from ${capitalizeFirstLetter(
           props.category
         )} `}
